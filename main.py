20fef8edb913db712c09df1268f4ab5c3b9111d7
Mrkdevv <aebolm@gmail.com>
2026-06-25 19:19:34 +0300

    added first WriteHeroMenu

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+
+from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLineEdit
+
+
+class main_window_to_write_hero(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HeroChoose")
+        self.setWindowIcon(QIcon("logo.jpg"))
+        self.setFixedSize(300, 300)
+        self.setStyleSheet("background-color:white;") #change in the future
+
+        # --
+
+        self.input = QLineEdit(self)
+        self.input.setFixedSize(300,50)
+        self.input.setPlaceholderText("Enter hero name")
+        self.input.setStyleSheet("background-color:black;")
+        self.input.returnPressed.connect(self.on_enter)
+
+        #--
+
+        font = QFont("Arial", 18)
+        font.setBold(True)
+        font.setItalic(True)
+        self.input.setFont(font)
+        self.input.setStyleSheet("background-color: black; color: white;")
+
+    def on_enter(self):
+        text = self.input.text().capitalize()
+        print(text)
+
+app = QApplication(sys.argv)
+
+window = main_window_to_write_hero()
+window.show()
+
+app.exec()
+
