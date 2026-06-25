7459f4822de0ed17dc3404a481d0991c0a39ebf7
Mrkdevv <aebolm@gmail.com>
2026-06-26 01:48:52 +0300

    First Version Completed! it ready to use

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
 
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QLabel
 from PyQt6.QtGui import QIcon, QFont
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLineEdit
+import sqlite3
 
+connection = sqlite3.connect('dota.db')
+cursor = connection.cursor()
 
 class main_window_to_write_hero(QMainWindow):
     def __init__(self):
@@ -29,9 +34,30 @@ class main_window_to_write_hero(QMainWindow):
         self.input.setFont(font)
         self.input.setStyleSheet("background-color: black; color: white;")
 
+        label_font = QFont("Arial", 20)
+        label_font.setBold(True)
+        font.setItalic(True)
+
+        self.label = QLabel(self)
+        self.label.setGeometry(-10, 60, 300, 200)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setFont(label_font)
+
     def on_enter(self):
         text = self.input.text().capitalize()
-        return text
+
+        cursor.execute("SELECT id from HEROES WHERE name = ?", (text,))
+        hero = cursor.fetchone()
+
+        if hero:
+            hero_id = hero[0]
+            cursor.execute("SELECT first, second, third from Counterpicks WHERE hero_id = ?", (hero_id,))
+            counterpicks = cursor.fetchone()
+            self.label.setText(f"1) {counterpicks[0]}\n 2) {counterpicks[1]}\n 3) {counterpicks[2]}\n")
+
+
+        else:
+            print("Hero not found")
 
 
 app = QApplication(sys.argv)
